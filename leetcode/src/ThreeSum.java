21bc874f8c41758de4a49ca16b7dcb467fa7b1b9
ShunyangLi <z5139935@student.unsw.edu.au>
2020-06-17 17:02:28 +1000

    finish three sum problem

--- leetcode/src/ThreeSum.java
+++ leetcode/src/ThreeSum.java
@@ -4,46 +4,51 @@ public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> list = new LinkedList<>();
         if (nums.length < 3) return list;
-        List<Integer> temp = null;
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i ++) {
-            int sub = 0 - nums[i];
-            if (temp != null) if (temp.contains(nums[i])) continue;
+        // assume the list is sorted
+        // then just make a loop
+        for (int i = 0; i < nums.length - 1; i ++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int num = -nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            int left = 0, right = nums.length - 1;
-            while (left != right) {
-                if (left == i){
-                    left ++;
+            while (left <= right) {
+                if (left == right) break;
+                if (left == i) {
+                    left++;
                     continue;
                 }
                 if (right == i) {
                     right --;
                     continue;
                 }
-                int sum = nums[left] + nums[right];
-
-                if (sum < sub) left ++;
-                else if (sum > sub) right --;
-                else {
-                    temp = new LinkedList<>();
-                    temp.add(nums[left]);
-                    temp.add(nums[right]);
-                    temp.add(nums[i]);
-
-                    Collections.sort(temp);
-                    if(list.contains(temp)) break;
+                if (nums[left] + nums[right] == num) {
+                    // then add numbers to the list
+                    List<Integer> temp = new LinkedList<Integer>(
+                            Arrays.asList(nums[i], nums[left], nums[right])
+                    );
                     list.add(temp);
-                    break;
+                    while (left < right && nums[left] == nums[left+1]) left ++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    left ++;
+                    right --;
+                } else if (nums[left] + nums[right] < num) {
+                    left ++;
+                } else {
+                    right --;
                 }
             }
         }
+
         return list;
     }
 
     public static void main(String[] args) {
         ThreeSum tm = new ThreeSum();
-        List<List<Integer>> list = tm.threeSum(new int[] {-1, 0, 1, 2, -1, -4});
+//        List<List<Integer>> list = tm.threeSum(new int[] {-1,0,1,2,-1,-4});
+        List<List<Integer>> list = tm.threeSum(new int[] {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6});
 
         for (List s: list) {
             System.out.println(s);
